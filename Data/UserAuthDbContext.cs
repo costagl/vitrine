58d17594e2a9297c91dfb15fe56b8758f4807e2f
costagl <glameiramc@gmail.com>
2025-05-16 16:46:32 -0300

    jwt implementado

---
+++ Data/UserAuthDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Vitrine.Models;
+
+namespace VitrineApi.Data
+{
+    public class UserAuthDbContext : IdentityDbContext<Lojista>
+    {
+        public UserAuthDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ ViewModels/RegisterViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VitrineApi.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Nome é necessário.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "E-mail é necessário.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Senha é necessário.")]
+        [StringLength(40, MinimumLength = 8)]
+        [DataType(DataType.Password)]
+        [Compare("ConfirmPassword", ErrorMessage = "Senhas não coincidem.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirmação de Senha é necessário.")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+        public string CPF { get; set; }
+        public string DataNascimento { get; set; }
+        public string CNPJ { get; set; }
+        public string NomeLoja { get; set; }
+        public string CategoriaVenda { get; set; }
+        public bool LembrarDeMim { get; set; }
+    }
+}
